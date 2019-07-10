e765f6a5a7df66c967075cbd9b13454eea1bbee2
chau vinh tuan <chau.vinh.tuan@linecorp.com>
2019-07-10 19:58:13 +0700

    Define new main activity

---
+++ app/src/main/java/com/tuanchauict/acb/ui/MainActivity2.kt
@@ -0,0 +1,12 @@
+package com.tuanchauict.acb.ui
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import com.tuanchauict.acb.R
+
+class MainActivity2 : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main_2)
+    }
+}
